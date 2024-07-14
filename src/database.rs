70f082fb179975ec2299807df9ff32d7055a4ad3
Luke Harding <lukeharding990@gmail.com>
2024-07-14 19:40:44 -0400

    Break command processing out into process.rs

--- src/database.rs
+++ src/database.rs
@@ -6,7 +6,6 @@
  */
 
 use sqlx::postgres::PgPoolOptions;
-use sqlx::PgPool;
 use tokio::sync::{mpsc, oneshot};
 use tokio::sync::mpsc::error::TryRecvError;
 use color_eyre::Result;
@@ -14,6 +13,7 @@ use uuid::Uuid;
 
 mod schema;
 mod cache;
+mod process;
 
 // This enum contains all possible commands that can be issued to the database
 pub enum DatabaseMpscCommand {
@@ -125,145 +125,6 @@ impl Database {
     }
 }
 
-pub async fn process_cmd(cmd: DatabaseMpscCommand, pool: &PgPool, cache: &mut cache::Cache) {
-    match cmd {
-        DatabaseMpscCommand::GetPage(path, skip_cache, reply) => {
-            if !skip_cache {
-                if let Some(result) = cache.get_page(&path).await {
-                    let _ = reply.send(Ok(result));
-                    return;
-                }
-            }
-
-            let page = match sqlx::query_as!(schema::Page, 
-                "SELECT * FROM pages WHERE path = $1", path)
-                .fetch_one(pool).await {
-                    Ok(page) => page,
-                    Err(err) => {
-                        let _ = reply.send(Err(err.into()));
-                        return;
-                    }
-                };
-            cache.set_page(&page).await;
-            let _ = reply.send(Ok(page));
-        },
-        DatabaseMpscCommand::SetPage(new_page, reply) => {
-            let result = sqlx::query!("UPDATE pages SET 
-                created_at = $1, 
-                created_by = $2, 
-                modified_at = $3, 
-                modified_by = $4, 
-                published = $5, 
-                body = $6 
-                WHERE \"path\" = $7", 
-                new_page.created_at, 
-                new_page.created_by, 
-                new_page.modified_at, 
-                new_page.modified_by, 
-                new_page.published, 
-                new_page.body, 
-                new_page.path
-                )
-                .execute(pool).await;
-            
-            if let Err(err) = result {
-                let _ = reply.send(Err(err.into()));
-            } else {
-                let _ = reply.send(Ok(()));
-                cache.set_page(&new_page).await;
-            }
-        },
-        DatabaseMpscCommand::DeletePage(path, reply) => {
-            let result = sqlx::query!("DELETE FROM pages WHERE \"path\" = $1", path)
-                .execute(pool).await;
-
-            if let Err(err) = result {
-                let _ = reply.send(Err(err.into()));
-            } else {
-                let _ = reply.send(Ok(()));
-            }
-        },
-        DatabaseMpscCommand::NewPage(new_page, reply) => {
-            let result = sqlx::query!("INSERT INTO pages VALUES($1, $2, $3, $4, $5, $6, $7)",
-                new_page.path, 
-                new_page.created_at, 
-                new_page.created_by,
-                new_page.modified_at, 
-                new_page.modified_by, 
-                new_page.published,
-                new_page.body)
-                .execute(pool).await;
-
-            if let Err(err) = result {
-                let _ = reply.send(Err(err.into()));
-            } else {
-                let _ = reply.send(Ok(()));
-            }
-        },
-        DatabaseMpscCommand::GetUser(id, skip_cache, reply) => {
-            if !skip_cache {
-                if let Some(user) = cache.get_user(id).await {
-                    let _ = reply.send(Ok(user));
-                    return;
-                }
-            }
-
-            let user = match sqlx::query_as!(schema::AdminUser, 
-                "SELECT * FROM admins WHERE id = $1", id)
-                .fetch_one(pool).await {
-                    Ok(user) => user,
-                    Err(err) => {
-                        let _ = reply.send(Err(err.into()));
-                        return;
-                    }
-                };
-            cache.set_user(&user).await;
-            let _ = reply.send(Ok(user));
-        },
-        DatabaseMpscCommand::SetUser(new_user, reply) => {
-            let result = sqlx::query!("UPDATE admins SET
-                username = $1,
-                enabled = $2,
-                email = $3
-                WHERE id = $4", 
-                new_user.username,
-                new_user.enabled,
-                new_user.email,
-                new_user.id)
-                .execute(pool).await;
-
-            if let Err(err) = result {
-                let _ = reply.send(Err(err.into()));
-            } else {
-                let _ = reply.send(Ok(()));
-            }
-        },
-        DatabaseMpscCommand::DeleteUser(id, reply) => {
-            let result = sqlx::query!("DELETE FROM admins WHERE id = $1", id)
-                .execute(pool).await;
-
-            if let Err(err) = result {
-                let _ = reply.send(Err(err.into()));
-            } else {
-                let _ = reply.send(Ok(()));
-            }
-        },
-        DatabaseMpscCommand::NewUser(new_user, reply) => {
-            let result = sqlx::query!("INSERT INTO admins VALUES($1, $2, $3, $4)",
-                new_user.id,
-                new_user.username,
-                new_user.enabled,
-                new_user.email)
-                .execute(pool).await;
-
-            if let Err(err) = result {
-                let _ = reply.send(Err(err.into()));
-            } else {
-                let _ = reply.send(Ok(()));
-            }
-        }
-    }
-}
 
 /// init_db()
 /// Initilize DB connection and return struct.
@@ -278,7 +139,7 @@ pub async fn init_db(database_url: String) -> Result<Database> {
         loop {
            match rx.try_recv() {
                Ok(cmd) => {
-                   process_cmd(cmd, &pool, &mut cache).await;
+                   process::cmd(cmd, &pool, &mut cache).await;
                },
                Err(err) => {
                    match err {
